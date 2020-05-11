2b81ee810db0559179d8c77799134509bc8bcfe6
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-05-11 14:43:00 -0500

    reducer file built

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+import React, { useState, useReducer } from 'react';
+
+const reducer = (state, action) => {
+
+};
+
+
+const initialState = {
+    
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export default reducer;
